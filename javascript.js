ed3f8a039a3f826eb6c6608c513bfe5175ad8f65
Haiyahhh <92042732+Haiyahhh@users.noreply.github.com>
2022-10-13 20:47:32 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let header2=document.getElementById("header2");
+let link=document.getElementById("link")
+let changeLink=()=>{
+    header2.style.backgroundColor="green"
+    link.style.backgroundColor="yellow"
+}
+let changeHeader=()=>{
+    link.style.background="green"
+    header2.style.backgroundColor="yellow"
+}
